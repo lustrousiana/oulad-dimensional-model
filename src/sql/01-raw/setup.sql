41cc7826fed1c1ba1f6aa6ae328afe8ad6bb403d
briana.capul@ftwfoundation.org <briana.capul@ftwfoundation.org>
2026-09-08 10:53:12 +0000

    deleted setup query

--- src/sql/01-raw/setup.sql
+++
@@ -1,5 +0,0 @@
--- Create catalog and schema if they don't exist
-CREATE CATALOG IF NOT EXISTS `ftw-week-07`;
-CREATE SCHEMA IF NOT EXISTS `ftw-week-07`.`01-raw`;
-CREATE SCHEMA IF NOT EXISTS `ftw-week-07`.`02-clean`;
-CREATE SCHEMA IF NOT EXISTS `ftw-week-07`.`03-mart`;
